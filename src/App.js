a8f763db77f403d3b2975ddd892d946cd2e83d51
tomhoon <gnsdl9108@naver.com>
2024-09-07 17:11:08 +0900

    feat: class화

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css'
+import R003_ImportComponent from './component/R003_ImportComponent'
 
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
       <p>HTML 적용하기</p>
+      <R003_ImportComponent/>
     </div>
   );
 }
